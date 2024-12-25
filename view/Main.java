3a1689d42f37c3c5906084b897b37a454f6c37b2
thanhcanhquanca <chuanhquan89@gmail.com>
2024-12-25 10:13:48 +0700

    kong tim thay lop dươc yeu cau (ClassNotFoundException)

--- view/Main.java
+++ view/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         // lỗi liên quan đến đầu vào và đầu ra như đọc , ghi tệp
         try {
@@ -28,7 +28,23 @@ public class Main {
             System.out.println("loi co so du lieu " + e.getMessage());
         }
 
-        //
+        // xảy ra khi chương tình không tìm thấy một lớp được yêu cầu
+
+        try {
+            Class.forName("com.unknow.ClassName");
+        }
+            catch (ClassNotFoundException e){
+                System.out.println("không tim thay lớp" + e.getMessage());
+            }
+
+        // xảy ra lỗi khi cố gắng mở tệp không tồn tai
+
+
+
 
     }
+
+
+
+
 }
