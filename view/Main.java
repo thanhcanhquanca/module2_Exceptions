8be38282e256d02cbefa7a05ed318276c35ec678
thanhcanhquanca <chuanhquan89@gmail.com>
2024-12-25 09:58:59 +0700

    loi I/O

--- view/Main.java
+++ view/Main.java
@@ -1,5 +1,21 @@
 package view;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) {
+
+        // lỗi liên quan đến đầu vào và đầu ra như đọc , ghi tệp
+        try {
+            FileReader file = new FileReader("nonexstent.txxt");
+            BufferedReader reader = new BufferedReader(file);
+            System.out.println(reader.readLine());
+        }catch (IOException e){
+            System.out.println("lỗi I/O xảy ra" + e.getMessage());
+        }
+
+        //
+    }
 }
