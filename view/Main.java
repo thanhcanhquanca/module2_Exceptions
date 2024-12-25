db4bfd9c6411fd87b8e3a5260d17df8fba71157e
thanhcanhquanca <chuanhquan89@gmail.com>
2024-12-25 10:04:33 +0700

    loi co so du lieu sql

--- view/Main.java
+++ view/Main.java
@@ -3,9 +3,10 @@ package view;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         // lỗi liên quan đến đầu vào và đầu ra như đọc , ghi tệp
         try {
@@ -16,6 +17,18 @@ public class Main {
             System.out.println("lỗi I/O xảy ra" + e.getMessage());
         }
 
+        // lỗi xảy ra khi làm việc với cơ sở dữ liệu  sql
+
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "user", "pass");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM nonexistent_table");
+
+        }catch (SQLException e){
+            System.out.println("loi co so du lieu " + e.getMessage());
+        }
+
         //
+
     }
 }
